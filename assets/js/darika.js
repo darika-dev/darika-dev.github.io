646c5dff6c8cd48575d2dcdd1ee32388be521c25
DariKa <ppartesia@gmail.com>
2015-04-27 19:58:36 +0500

    reDesing

---
+++ assets/js/darika.js
@@ -0,0 +1,226 @@
+/*! DariKa v1.0 | (c) 2015 DariKa
+*/
+jQuery(window).load(function() {
+    jQuery("#loader").fadeOut("slow");
+    equalheight(".skills .inner");
+    var e = 0;
+    var d = $(window).height();
+    var b = $(window).width();
+    var c = d - 100;
+    $(".home, .home .text").css("height", d);
+    var a = d / 2;
+    $(".flex-direction-nav a").css("top", (a + "px"));
+    e = $(this).scrollTop();
+    if (e > 20) {
+        $("header").stop().animate({
+            backgroundColor: "#111111"
+        }, 300)
+    } else {
+        $("header").stop().animate({
+            backgroundColor: "transparent"
+        }, 300)
+    }
+});
+$(document).ready(function() {
+    $(".home button").click(function(j) {
+        var i = $(".about").offset().top;
+        $("html,body").animate({
+            scrollTop: i - 70
+        }, "slow");
+        return false
+    });
+    $("#navigation ul a, .logo a, .arrow a").click(function(l) {
+        var j = $(window).width();
+        if (j <= 767) {
+            $("#navigation ul").slideUp("fast")
+        }
+        var i = $(this).attr("href");
+        var k = $(i).offset().top;
+        $("html,body").animate({
+            scrollTop: k - 70
+        }, "slow");
+        return false
+    });
+    $(".social li").click(function() {
+        var i = $(this).attr("data-url");
+        window.open(i, "_blank")
+    });
+    var h = 0;
+    var g = $(window).height();
+    var f = g - 100;
+    $(".home").css("height", f);
+    $(".home .overlay").css("height", g);
+    $(document).scroll(function() {
+        h = $(this).scrollTop();
+        if (h > 20) {
+            $("header").stop().animate({
+                backgroundColor: "#111111"
+            }, 300)
+        } else {
+            $("header").stop().animate({
+                backgroundColor: "transparent"
+            }, 300)
+        }
+    });
+    var d = "15%";
+    $("section").waypoint({
+        handler: function(j, k) {
+            var i = $(this);
+            if (k === "up") {
+                i = i.prev()
+            }
+            if (k === "up") {
+                d = "30%"
+            }
+            if (typeof i.attr("id") != "undefined") {
+                $("#navigation a").removeClass("active");
+                $('#navigation a[href="#' + i.attr("id") + '"]').addClass("active")
+            }
+        },
+        offset: d
+    });
+    $('#contact').waypoint(function(j, k) {
+    	if (k === "down") {
+		  	$("#navigation a").removeClass("active");
+    	    $('#navigation li:last-child a').addClass("active")
+    	} else {
+    		$("#navigation a").removeClass("active");
+    	    $('#navigation li:nth-last-child(2) a').addClass("active")
+    	};
+	}, { offset: 'bottom-in-view' });
+
+
+
+
+    $(".logo").click(function(j) {
+        var i = $("#home").offset().top;
+        $("html,body").animate({
+            scrollTop: i - 70
+        }, "slow", "easeOutQuint");
+        return false
+    });
+    
+    var e = false;
+    $(".mobile-navigation").click(function() {
+        if (e == false) {
+            $("#navigation ul").slideDown("fast");
+            $("header").stop().animate({
+                backgroundColor: "#111111"
+            }, 300);
+            e = true;
+            return false
+        } else {
+            $("#navigation ul").slideUp("fast");
+            h = $(window).scrollTop();
+            if (h > 20) {
+                $("header").stop().animate({
+                    backgroundColor: "#111111"
+                }, 300)
+            } else {
+                $("header").stop().animate({
+                    backgroundColor: "transparent"
+                }, 300)
+            }
+            e = false;
+            return false
+        }
+    });
+    
+    var c = $("div.skills");
+    $("div.skills").hover(function() {
+        c.stop().animate({
+            opacity: 0.3
+        }, "fast");
+        $(this).stop().animate({
+            opacity: 1
+        }, "fast")
+    }, function() {
+        c.stop().animate({
+            opacity: 1
+        }, "fast")
+    });
+
+
+	var window_width = $(window).width();
+    if (window_width >= 960) {
+	    $(".work .info").css({
+	        opacity: 0
+	    });
+
+	    $(".work a").hover(function() {
+	        $(this).children(".info ").stop().animate({
+	            opacity: 1
+	        }, "fast")
+	    }, function() {
+	        $(this).children(".info ").stop().animate({
+	            opacity: 0
+	        }, "slow")
+	    });
+	};
+	var a = ($(".work").height() - $(".work .inner").height() - 16) / 2;
+    $(".work .inner").css("margin-top", a + "px");
+
+    $(".work a").click(function() {
+        var i = $(this).attr("data-url");
+        $.ajax({
+            url: i
+        }).success(function(j) {
+            $(".work-preview").fadeIn("fast");
+            $(".work-preview").html(j);
+            $("html,body").animate({
+                scrollTop: $(".work-preview").offset().top - 70
+            }, 500);
+            $(".work-preview-close").click(function() {
+                $(".work-preview").fadeOut("fast");
+                $("html,body").animate({
+                    scrollTop: $(".works").offset().top - 70
+                }, 500);
+                setTimeout(function() {
+                    $(".work-preview").html("")
+                }, 1000)
+            });
+            $(".work-preview-slider").flexslider({
+                animation: "slide",
+                slideshow: true,
+                directionNav: false,
+                controlNav: true,
+                animationSpeed: 600
+            })
+        });
+        return false
+    });
+    $(".work-preview-close").click(function() {
+        $("#work-preview").fadeOut();
+        $("#work-preview").empty();
+        var i = $(".works").offset().top;
+        $("html,body").scrollTop(i - 70);
+        return false
+    })
+});
+$(window).resize(function() {
+    equalheight(".skills .inner")
+});
+equalheight = function(b) {
+    var a = 0, c = 0, f = new Array(), e, d = 0;
+    $(b).each(function() {
+        e = $(this);
+        $(e).height("auto");
+        topPostion = e.position().top;
+        if (c != topPostion) {
+            for (currentDiv = 0; currentDiv < f.length; currentDiv++) {
+                f[currentDiv].height(a)
+            }
+            f.length = 0;
+            c = topPostion;
+            a = e.height();
+            f.push(e)
+        } else {
+            f.push(e);
+            a = (a < e.height()) ? (e.height()) : (a)
+        }
+        for (currentDiv = 0; currentDiv < f.length; currentDiv++) {
+            f[currentDiv].height(a)
+        }
+    })
+};
+
---
+++ assets/js/main.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+	$('body').scrollspy({ 
+		target: '#navbar' 
+	});
+})
+
+
+var data = {
+	"1": {
+		"img": "img/nakleeno.png",
+		"title": "Nakleeno",
+		"tools": "Bootstrap, CSS, JavaScript",
+		"des": "Интернет-магазин наклеек",
+		"url": ""
+	},
+	"2": {
+		"img": "img/asper.png",
+		"title": "Asper",
+		"tools": "HTML5, Less",
+		"des": "Сервис по подбору персонала",
+		"url": ""
+	},
+	"3": {
+		"img": "img/newproject.png",
+		"title": "NewProject1",
+		"tools": "Дизайн, Jade, Less, jQuery, Node.js",
+		"des": "Корпоративный сайт",
+		"url": "http://newproject1.com/"
+	},
+	"4": {
+		"img": "img/playkey.png",
+		"title": "PlayKey",
+		"tools": "HTML5, Stylus, jQuery",
+		"des": "Сервис облачных игр",
+		"url": "http://playkey.net/"
+	},
+	"5": {
+		"img": "img/leolibro.png",
+		"title": "LeoLibro",
+		"tools": "HTML5, Stylus, jQuery",
+		"des": "Интернет-магазин электронных книг",
+		"url": ""
+	},
+	"6": {
+		"img": "img/elis.png",
+		"title": "Elis-строй",
+		"tools": "Дизайн, HTML5, Stylus, jQuery",
+		"des": "Сайт строительной компании",
+		"url": ""
+	},
+	"7": {
+		"img": "img/plitka.png",
+		"title": "Студия плитки",
+		"tools": "HTML5, Stylus, jQuery",
+		"des": "Интернет-магазин керамической плитки",
+		"url": ""
+	},
+	"8": {
+		"img": "img/sm_site.png",
+		"title": "Городские встречи",
+		"tools": "Дизайн, HTML5, Stylus, Wordpress",
+		"des": "Сайт и блог арт-агенства",
+		"url": ""
+	},
+	"9": {
+		"img": "img/simakov.png",
+		"title": "Simakov.it",
+		"tools": "Редизайн, Bootstrap, css, jQuery",
+		"des": "Портфолио мобального разработчика",
+		"url": "http://simakov.it/"
+	}
+}
